53cd163152c0eb75e6552c54ead0d7834f4adf02
chaphill <claycarroll7@gmail.com>
2020-01-09 17:08:48 -0500

    updated javascript file

--- script.js
+++ script.js
@@ -1,7 +1,67 @@
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  secondOperand: false,
+  operator: null
+}
+
+function inputNums(num){
+  const {displayValue} = calculator;
+  calculator.displayValue = displayValue == '0' ? num : displayValue + num;
+}
+
+function inputDecimal(deci){
+  if(calculator.displayValue.includes('.')){
+    return;
+  }
+  calculator.displayValue += deci;
+}
+
+
+
+function updateDisplay(){
+  const display = document.querySelector("#display");
+  if(display.innerHTML.length >= 30){
+    return;
+  }
+  display.innerText = calculator.displayValue;
+
+}
+
+
+
+updateDisplay();
+
+const keys = document.querySelector('#container');
+keys.addEventListener('click', e => {
+  const target = e.target;
+  if(!target.matches('button')) {
+    return;
+  }
+  if(target.classList.contains('operator')){
+    console.log('operator', target.innerHTML);
+    return;
+  }
+  if(target.classList.contains('decimal')){
+    inputDecimal(target.innerHTML);
+    updateDisplay();
+    return;
+  }
+  if(target.classList.contains('clear')){
+    console.log('clear', target.innerHTML);
+    return;
+  }
+
+  inputNums(target.innerHTML);
+  updateDisplay();
+});
+
+
 const add = (a, b) => a+b;
 const multiply = (a, b) => a*b;
 const subtract = (a, b) => a-b;
 const divide = (a, b) => a/b;
+const plusMinus = (a) => a *= -1;
 
 function operate(operator, a, b){
   switch(operator){
@@ -17,10 +77,10 @@ function operate(operator, a, b){
     case '/':
       return divide(a, b);
       break;
+    case '+/-':
+      return plusMinus(a);
+      break;
     default:
       return "enter valid operator";
   }
 }
-
-const display = document.querySelector("#display");
-display.innerHTML = operate("+", 10, 20);
